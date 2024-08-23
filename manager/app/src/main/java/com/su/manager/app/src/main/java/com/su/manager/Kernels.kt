1be07a1191b90238d801efbee42e48075f7ab342
smile349 <122448836+smile349@users.noreply.github.com>
2024-08-23 15:44:35 +0800

    Update Kernels.kt

--- manager/app/src/main/java/com/su/manager/app/src/main/java/com/su/manager/Kernels.kt
+++ manager/app/src/main/java/com/su/manager/app/src/main/java/com/su/manager/Kernels.kt
@@ -13,17 +13,7 @@ data class KernelVersion(val major: Int, val patchLevel: Int, val subLevel: Int)
     }
 
     fun isGKI(): Boolean {
-
-        // kernel 6.x
-        if (major > 5) {
-            return true
-        }
-
-        // kernel 5.10.x
-        if (major == 5) {
-            return patchLevel >= 10
-        }
-
+        // 永远返回 false
         return false
     }
 }
